89093a9527767c9cf08a0de760b72966d7982bb9
Rex Low <qiweilow950823@gmail.com>
2017-11-02 21:30:51 +0800

    updated

--- login.php
+++ login.php
@@ -13,28 +13,33 @@
 </head>
 
 <body>
-    <div class="uk-section uk-container uk-text-center">
-        <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid="">
-            <form>
-                <fieldset class="uk-fieldset">
+<div class="uk-section uk-container">
+  		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+			<form class="uk-form-stacked js-login">
+				
+				<h2>Login</h2>
 
-                    <legend class="uk-legend">Login</legend>
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Email</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
+			        </div>
+			    </div>
 
-                    <div class="uk-margin">
-                        <input class="uk-input" type="text" placeholder="john@gmail.com">
-                    </div>
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Password</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your Password">
+			        </div>
+			    </div>
 
-                    <div class="uk-margin">
-                        <input class="uk-input" type="text" placeholder="password">
-                    </div>
+			    <div class="uk-margin">
+			        <button class="uk-button uk-button-default" type="submit">Login</button>
+			    </div>
 
-                    <div class="uk-margin">
-                        <a class="uk-button uk-button-default" href="#">Login</a>
-                    </div>
-                </fieldset>
-            </form>
-        </div>
-    </div>
+			</form>
+  		</div>
+  	</div>
     <?php require_once "inc/footer.php" ?>
 </body>
 
--- register.php
+++ register.php
@@ -13,28 +13,35 @@
 </head>
 
 <body>
-    <div class="uk-section uk-container uk-text-center">
-        <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid="">
-            <form>
-                <fieldset class="uk-fieldset">
+<div class="uk-section uk-container">
+  		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+			<form class="uk-form-stacked js-register">
+				
+				<h2>Register</h2>
 
-                    <legend class="uk-legend">Registration</legend>
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Email</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
+			        </div>
+			    </div>
 
-                    <div class="uk-margin">
-                        <input class="uk-input" type="text" placeholder="john@gmail.com">
-                    </div>
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Passphrase</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your passphrase">
+			        </div>
+			    </div>
 
-                    <div class="uk-margin">
-                        <input class="uk-input" type="text" placeholder="password">
-                    </div>
+			    <div class="uk-margin uk-alert uk-alert-danger js-error" style='display: none;'></div>
 
-                    <div class="uk-margin">
-                        <a class="uk-button uk-button-default" href="#">Register</a>
-                    </div>
-                </fieldset>
-            </form>
-        </div>
-    </div>
+			    <div class="uk-margin">
+			        <button class="uk-button uk-button-default" type="submit">Register</button>
+			    </div>
+
+			</form>
+  		</div>
+  	</div>
     <?php require_once "inc/footer.php" ?>
 </body>
 
