76cfe775c9427da5cd65c6265c0d07d6fd4dc051
Rex Low <qiweilow950823@gmail.com>
2017-11-02 22:03:05 +0800

    added simple validation for registration

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,23 @@ $(document).on("submit", "form.js-register", function(event) {
   event.preventDefault();
 
   var form = $(this);
+  var error = $('.js-error', form);
   var data = {
     email: $("input[type='email']", form).val(),
     password: $("input[type='password']", form).val(),
   }
 
-  console.log(data);
+  // validation
+  if (data.email.length < 6) {
+    error.text("Please enter a valid email address").show();
+    return false;
+  } else if (data.password.length < 11) {
+    error.text("Please enter a passphrase that is at least 11 characters long").show();
+    return false
+  }
+
+  // ajax
+  error.hide(); // hide error text when password length is more than 11
 
   return false;
 })
