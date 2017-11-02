309e3a1ed65fc60a2efdf593f21ff96fa289284a
Rex Low <qiweilow950823@gmail.com>
2017-11-02 21:00:11 +0800

    added jquery and link to js folder

--- inc/footer.php
+++ inc/footer.php
@@ -4,3 +4,9 @@
 <!-- UIkit JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.31/js/uikit.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.31/js/uikit-icons.min.js"></script>
+
+<!-- jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- JavaScripts -->
+<script src="./assets/js/main.js"></script>
