32d557f490906e940a5e073efe1864a9c77c8883
Rex Low <qiweilow950823@gmail.com>
2017-11-03 23:16:53 +0800

    redirect user to login page if session is not found

--- dashboard.php
+++ dashboard.php
@@ -1,8 +1,12 @@
 <?php 
     define('__CONFIG__', true);     // allow the configs
     require_once "inc/config.php";  // require the configs
-    echo $_SESSION['user_id'] . ' is your user id';
-    exit;
+    
+    if (isset($_SESSION['user_id'])) {
+      
+    } else {
+      header("Location: ./login.php"); exit;
+    }
 ?>
 
 <html lang="en">
@@ -11,16 +15,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login Registration</title>
+    <title>Dashboard</title>
 </head>
 
 <body>
     <div class="uk-section uk-container uk-text-center">
-        <!-- <?php echo "Hello world! Today is "; echo date("Y m d"); ?> -->
-        <div class="uk-margin">
-            <a class="uk-button uk-button-primary" href="./login.php">Login</a>
-            <a class="uk-button uk-button-secondary" href="./register.php">Register</a>
-        </div>
+        <?php echo "Hello world! Today is "; echo date("Y m d"); ?>
     </div>
     <?php require_once "inc/footer.php" ?>
 </body>
