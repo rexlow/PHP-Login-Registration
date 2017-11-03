a24db69882401d301aef7d912b25dfba2d4e7fdd
Rex Low <qiweilow950823@gmail.com>
2017-11-03 22:52:44 +0800

    created dashboard page

---
+++ dashboard.php
@@ -0,0 +1,28 @@
+<?php 
+    define('__CONFIG__', true);     // allow the configs
+    require_once "inc/config.php";  // require the configs
+    echo $_SESSION['user_id'] . ' is your user id';
+    exit;
+?>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login Registration</title>
+</head>
+
+<body>
+    <div class="uk-section uk-container uk-text-center">
+        <!-- <?php echo "Hello world! Today is "; echo date("Y m d"); ?> -->
+        <div class="uk-margin">
+            <a class="uk-button uk-button-primary" href="./login.php">Login</a>
+            <a class="uk-button uk-button-secondary" href="./register.php">Register</a>
+        </div>
+    </div>
+    <?php require_once "inc/footer.php" ?>
+</body>
+
+</html>
