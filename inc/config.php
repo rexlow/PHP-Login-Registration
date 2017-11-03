459e6fd6c56663632e7ce290fae4825538de7e42
Rex Low <qiweilow950823@gmail.com>
2017-11-03 21:22:20 +0800

    fixed a type

--- inc/config.php
+++ inc/config.php
@@ -4,6 +4,9 @@
     exit('You do not have a config file');
   }
 
+  error_reporting(-1);
+  ini_set('display_errors', 'On');
+
   // include relevant configs
   include_once "classes/DB.php";
   include_once "classes/Filter.php";
