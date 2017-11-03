ab2b5cc34e2dbf2f03456905211afd8eebb76918
Rex Low <qiweilow950823@gmail.com>
2017-11-03 18:02:01 +0800

    some more ajax

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,22 +4,44 @@ $(document).on("submit", "form.js-register", function(event) {
 
   var form = $(this);
   var error = $('.js-error', form);
-  var data = {
+  var dataObject = {
     email: $("input[type='email']", form).val(),
     password: $("input[type='password']", form).val(),
   }
 
   // validation
-  if (data.email.length < 6) {
+  if (dataObject.email.length < 6) {
     error.text("Please enter a valid email address").show();
     return false;
-  } else if (data.password.length < 11) {
+  } else if (dataObject.password.length < 11) {
     error.text("Please enter a passphrase that is at least 11 characters long").show();
     return false
   }
 
-  // ajax
   error.hide(); // hide error text when password length is more than 11
+
+  // ajax
+  $.ajax({
+    type: 'POST',
+    url: './ajax/register.php',
+    data: dataObject,
+    dataType: 'json',
+    async: true
+  })
+  .done((data) => {
+    console.log(data)
+    if (data.redirect !== undefined) {
+      window.location = data.redirect;
+    }
+
+    alert(data.name);
+  })
+  .fail((e) => {
+    console.log(e)
+  })
+  .always((data) => {
+    console.log(data)
+  })
 
   return false;
 })
