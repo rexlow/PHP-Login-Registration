cee6749c2c5d60681e61b5084d8960cd4319ed3f
Rex Low <qiweilow950823@gmail.com>
2017-11-03 22:51:35 +0800

    added a ternary condition to redirect ajax call for both login and register

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 // bind all our listeners to DOM
-$(document).on("submit", "form.js-register", function (event) {
+$(document).on("submit", "form.js-register, form.js-login", function(event) {
   event.preventDefault();
 
   var form = $(this);
@@ -13,8 +13,8 @@ $(document).on("submit", "form.js-register", function (event) {
   if (dataObject.email.length < 6) {
     error.text("Please enter a valid email address").show();
     return false;
-  } else if (dataObject.password.length < 11) {
-    error.text("Please enter a passphrase that is at least 11 characters long").show();
+  } else if (dataObject.password.length < 8) {
+    error.text("Please enter a passphrase that is at least 8 characters long").show();
     return false
   }
 
@@ -23,13 +23,12 @@ $(document).on("submit", "form.js-register", function (event) {
   // ajax
   $.ajax({
       type: 'POST',
-      url: './ajax/register.php',
+      url: (form.hasClass("js-login") ? './ajax/login.php' : './ajax/register.php'),
       data: dataObject,
       dataType: 'json',
       async: true
     })
     .done((data) => {
-      console.log(data)
       if (data.redirect !== undefined) {
         window.location = data.redirect;
       } else if (data.error !== undefined) {
@@ -45,3 +44,49 @@ $(document).on("submit", "form.js-register", function (event) {
 
   return false;
 })
+
+// .on("submit", "form.js-login", function(event) {
+//   event.preventDefault();
+
+//   var form = $(this);
+//   var error = $('.js-error', form);
+//   var dataObject = {
+//     email: $("input[type='email']", form).val(),
+//     password: $("input[type='password']", form).val(),
+//   }
+
+//   // validation
+//   if (dataObject.email.length < 6) {
+//     error.text("Please enter a valid email address").show();
+//     return false;
+//   } else if (dataObject.password.length < 8) {
+//     error.text("Please enter a passphrase that is at least 11 characters long").show();
+//     return false
+//   }
+
+//   error.hide(); // hide error text when password length is more than 11
+
+//   // ajax
+//   $.ajax({
+//       type: 'POST',
+//       url: './ajax/login.php',
+//       data: dataObject,
+//       dataType: 'json',
+//       async: true
+//     })
+//     .done((data) => {
+//       if (data.redirect !== undefined) {
+//         window.location = data.redirect;
+//       } else if (data.error !== undefined) {
+//         error.html(data.error).show();
+//       }
+//     })
+//     .fail((e) => {
+//       console.log(e)
+//     })
+//     .always((data) => {
+//       console.log(data)
+//     })
+
+//   return false;
+// })
