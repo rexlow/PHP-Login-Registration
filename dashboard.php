74709d4525a42fc51656f04c53b89ce31dcaca02
Rex Low <qiweilow950823@gmail.com>
2017-11-04 00:13:43 +0800

    check is user still exist to display data, otherwise force logout

--- dashboard.php
+++ dashboard.php
@@ -3,6 +3,25 @@
     require_once "inc/config.php";
     
     checkIfUserIsLoggedIn();
+
+    $user_id = $_SESSION['user_id'];
+
+    $getUserInfo = $con->prepare("SELECT email, reg_time FROM users WHERE user_id = :user_id LIMIT 1");
+    $getUserInfo->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+    $getUserInfo->execute();
+
+    /*
+     * maybe some point in the time user is deleted by admin, but user still has session in his browser
+     * he will be still able to access to data
+     * so we need to check is user still exist to display data, otherwise force logout
+     */
+    if ($getUserInfo->rowCount() == 1) {
+        // user was found
+        $User = $getUserInfo->fetch(PDO::FETCH_ASSOC);
+    } else {
+        // user is not signed in
+        header("Location: ./logout.php"); exit;
+    }
 ?>
 
 <html lang="en">
@@ -16,7 +35,8 @@
 
 <body>
     <div class="uk-section uk-container uk-text-center">
-        <?php echo "Hello world! Today is "; echo date("Y m d"); ?>
+        <p>Hello <?php echo $User['email']; ?>, you registered at <?php echo $User['reg_time']; ?></p>
+        <p><a href="./logout.php">Logout</a></p>
     </div>
     <?php require_once "inc/footer.php" ?>
 </body>
