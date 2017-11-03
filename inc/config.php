d64400f2bfc14558bc0bc12f981e7b2ab274c2db
Rex Low <qiweilow950823@gmail.com>
2017-11-03 22:23:22 +0800

    added session start

--- inc/config.php
+++ inc/config.php
@@ -1,13 +1,27 @@
 <?php 
-  // if there is no constant defined called __CONFIG__, do not load this file
+  /*
+   * if there is no constant defined called __CONFIG__, do not load this file
+   */
   if (!defined('__CONFIG__')) {
     exit('You do not have a config file');
   }
 
+  /*
+   * Always start session
+   */
+  if (!isset($_SESSION)) {
+    session_start();
+  }
+
+  /*
+   * allow error reporting
+   */
   error_reporting(-1);
   ini_set('display_errors', 'On');
 
-  // include relevant configs
+  /*
+   * include relevant configs
+   */
   include_once "classes/DB.php";
   include_once "classes/Filter.php";
 
